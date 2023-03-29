81eb572da0787e23782347942fc5fe42aa22c7a8
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-29 15:53:58 +0200

    chore: update deps

--- src/update-files.ts
+++ src/update-files.ts
@@ -1,4 +1,4 @@
-import * as path from 'path'
+import * as path from 'node:path'
 import { readJsonFile, readTextFile, writeJsonFile, writeTextFile } from './fs'
 import { isManifest } from './manifest'
 import type { Operation } from './operation'
