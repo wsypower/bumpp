032bd7a8033013f16988743e1a2cfe3001016427
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-03-04 13:50:48 -0500

    feat: find config in parent directory (#8)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 import process from 'node:process'
+import { dirname } from 'node:path'
 import { loadConfig } from 'c12'
+import escalade from 'escalade/sync'
 import type { VersionBumpOptions } from './types/version-bump-options'
 
 export const bumpConfigDefaults: VersionBumpOptions = {
@@ -18,23 +20,49 @@ export async function loadBumpConfig(
   overrides?: Partial<VersionBumpOptions>,
   cwd = process.cwd(),
 ) {
-  const { config: bumppConfig } = await loadConfig<VersionBumpOptions>({
-    name: 'bumpp',
+  const name = 'bump'
+  const configFile = findConfigFile(name, cwd)
+  const { config } = await loadConfig<VersionBumpOptions>({
+    name,
+    defaults: bumpConfigDefaults,
     overrides: {
       ...(overrides as VersionBumpOptions),
     },
-    cwd,
+    cwd: configFile ? dirname(configFile) : cwd,
   })
-  const { config } = await loadConfig<VersionBumpOptions>({
-    name: 'bump',
-    defaults: bumpConfigDefaults,
-    overrides: {
-      ...(bumppConfig!),
-    },
-    cwd,
-  })
-
   return config!
+}
+
+function findConfigFile(name: string, cwd: string) {
+  let foundRepositoryRoot = false
+  try {
+    const candidates = ['js', 'mjs', 'ts', 'mts', 'json'].map(ext => `${name}.config.${ext}`)
+    return escalade(cwd, (_dir, files) => {
+      const match = files.find((file) => {
+        if (candidates.includes(file))
+          return true
+        if (file === '.git')
+          foundRepositoryRoot = true
+        return false
+      })
+
+      if (match)
+        return match
+
+      // Stop at the repository root.
+      if (foundRepositoryRoot) {
+        // eslint-disable-next-line no-throw-literal
+        throw null
+      }
+
+      return false
+    })
+  }
+  catch (error) {
+    if (foundRepositoryRoot)
+      return null
+    throw error
+  }
 }
 
 export function defineConfig(config: Partial<VersionBumpOptions>) {
